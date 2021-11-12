569efb7416347b8ca264e66037adb3ca02cd5e8a
Alen767 <76885549+Alen767@users.noreply.github.com>
2021-11-12 11:34:26 +0300

    Update js 12.11.2021.js

--- js 12.11.2021.js
+++ js 12.11.2021.js
@@ -1,1 +1,59 @@
-let 
+
+
+
+
+
+
+let car = {
+  speed: 100,
+  brand: "Tesla",
+  hp: 10,
+  model: "Ru-Edition",
+  automat: false,
+  user: {
+    name: "kesha Mask",
+    number: "s111ad111"
+      },
+  color: "баклжан",
+  getInfo: function() { //метод обьекта
+    return `${car.brand} ${this.model} цвета ${this.color} принадлежит лицу,именинуего ,с именем ${this.user.name}`
+    }
+  };
+               
+               
+               
+               
+               
+let car2= {
+  speed: 10,
+  brand: "Lada"
+  hp: 1.5
+  model: "Donald-duck"
+  automat: false,
+  user: {
+    name: "kesha Mask",
+    number: "s111ad111"
+      },
+  color: "радужный",
+  getInfo: function() { //метод обьекта
+    return `${car.brand} ${car.model} цвета ${$car.color} принадлежит лицу,именинуего ,с именем ${car.user.name}`
+    }
+  };
+document.write('<h3>${car.getInfo()}</h3>`)
+document.write('<h3>${car.getInfo()}</h3>`)
+               
+               
+               
+let rectangle = {
+weight: 100,
+height: 100,
+method: {
+  met1: "simpson",
+  met2: "gousa",
+  }
+};
+getInfo: function() {
+  return `${rectangle.met2}Прямоугольник 20x5 имеет периметр 50 и площадь 100}`
+               
+
+               
